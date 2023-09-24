4807925ddcc5c469bb7174ccead43a9cdb03ce86
starDev <gooddev123123@gmail.com>
2023-09-24 10:38:20 -0400

    add file

---
+++ admin-dash/src/page/list/List.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './list.scss';
+
+const List = () => {
+  return (
+    <div className="List">
+      <h1>List</h1>
+    </div>
+  );
+};
+
+export default List;
---
+++ admin-dash/src/page/single/Single.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './home.scss';
+
+const Single = () => {
+  return (
+    <div className="home">
+      <h1>Single</h1>
+    </div>
+  );
+};
+
+export default Single;
